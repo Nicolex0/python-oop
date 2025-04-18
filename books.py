fd95913bbf9df89cabb8a5fa89284085a172e4d7
Nicole Apono <napononicole@gmail.com>
2025-04-18 17:24:20 +0300

    adding method that shows book details and status

--- books.py
+++ books.py
@@ -7,4 +7,6 @@ class Book:
         self.genre = genre # Genre of the book
         self.is_checked_out = False #Checks whether the book is borrowed. If false, the book is available.
 
-    
+    def get_details(self):
+        status = "Available" if not self.is_checked_out else "Checked Out" # Checks if book is checked out and sets the status text
+        return f"Title: {self.title}, Author: {self.author}, Pages: {self.pages}, Genre: {self.genre}, Status: {status}" # status text
