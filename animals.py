31fcd48353412a9b9033b55bcc516cd2ab59f69f
Nicole Apono <napononicole@gmail.com>
2025-04-18 21:09:20 +0300

    adding child classes

--- animals.py
+++ animals.py
@@ -5,3 +5,20 @@ class Animal:
 
     def move(self):
         return f"{self.name} is moving."
+    
+# Child classes
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name) # Calls the constructor of the parent class
+        self.breed = breed
+
+    def move(self):
+        return f"{self.name} is running."# Overriding the parent class method
+    
+class Fish(Animal):
+    def __init__(self, name, species):
+        super().__init__(name) # Calls the constructor of the parent class
+        self.species = species
+
+    def move(self):
+        return f"{self.name} is swimming."# Overriding the parent class method
